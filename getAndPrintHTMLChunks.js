fda13fa3af2645e3b16ee0ee8502fb6a69dca40c
Kate Connolly <katherine.d.connolly@gmail.com>
2018-10-29 21:14:29 +0000

    properly did the first step for real.

--- getAndPrintHTMLChunks.js
+++ getAndPrintHTMLChunks.js
@@ -1,26 +1,19 @@
 var https = require("https");
 
-function getAndPrintHTMLChunks() {
+var https = require('https');                               
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: "/http-examples/step1.html"
+  path: '/http-examples/step1.html'
 };
 
-https.get(requestOptions, function(response) {
-  // set encoding of received data to UTF-8
-  response.setEncoding("utf8");
-
-  response.on("data", function(data) {
-    console.log("Chunk Received. Length:", data.length);
+https.get(requestOptions, function (response) {
+  response.setEncoding('utf8');
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data.length);
+  });
+  response.on('end', function() {
+    console.log('Response stream complete.');
   });
 
-  // the callback is invoked when all of the data has been received
-  // (the `end` of the stream)
-  response.on("end", function() {
-    console.log("Response stream complete.");
-  });
 });
-
-}
-console.log(getAndPrintHTMLChunks());
