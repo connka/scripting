cd823d9b4653f6cccbb923b7445a0d0d3c4c2bec
Kate Connolly <katherine.d.connolly@gmail.com>
2018-10-30 00:07:30 +0000

    Step 4 & 5

---
+++ getHTML.js
@@ -0,0 +1,26 @@
+var https = require("https");
+
+module.exports = {getHTML, printHTML};
+
+function getHTML (options, callback) {
+  https.get(options, function(response) {
+    var body = "";
+    response.on("data", chunk => {
+      body += chunk;
+    });
+
+    response.on("end", () => {
+      callback(body);
+    });
+  });
+}
+
+
+function printHTML (html) {
+    console.log(html);
+  }
+  
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+  };
